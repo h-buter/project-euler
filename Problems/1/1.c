30ec2536f660695bd16f5ad7055a74c9be477f9c
huub <112480089+h-buter@users.noreply.github.com>
2025-06-30 12:25:29 +0200

    Solved first problem!

---
+++ Problems/1/1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>  
+
+const int max = 1000;
+int total = 0;
+int multiples[] = {3,5};
+
+char problemNumber[] = __FILE_NAME__;
+
+
+int main() 
+{  
+    printf("Problem number %c: Multiples of 3 or 5\n", problemNumber[0]);
+    
+    for (int i = 1; i <= max-1; i++) // Loop over all values minus 1 because of "under x"
+    {
+        for (unsigned long j = 0; j < sizeof(multiples) / sizeof(multiples[0]); j++) // Loop over all multiple checks with a second for loop for the amount of multiples
+        {
+            if (i % multiples[j] == 0) // Check the modulo of the multiples
+            {
+                // printf("i: %i, j: %li, multiples: %i\n", i, j, multiples[j]);
+                total += i; // Sum the values
+                break; // Break the multiples for loop other wise doubles, for example mod of 3 and 5 are both true for 15, so skip checking the other modulos when one is true
+            }
+        }
+    }
+
+    printf("Result: %i\n", total);
+
+    return 0;  
+}
