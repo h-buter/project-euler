d96f87da2fd049e211e912145fe5334196be69b8
huub <112480089+h-buter@users.noreply.github.com>
2025-07-14 21:31:26 +0200

    rewritten the printproblemnumber function to allow files with more than 1 digit to be printed correctly

--- Problems/common.c
+++ Problems/common.c
@@ -1,6 +1,30 @@
 #include <stdio.h>  
+#include <stdbool.h>
+#include <math.h>
 
 void printProblemNumber(char fileName[], char* problemName)
 {
-    printf("Problem number %c: %s\n", fileName[0], problemName);
+    bool found = false;
+    int i = 0;
+    while(!found)
+    {
+        if(fileName[i] == '.')
+        {
+            found = true;
+            i--;
+        }
+        else 
+        {
+            i++;
+        }
+    }
+    
+    int problemNumber = 0;
+    for(int j = 0; j <= i; j++)
+    {
+        problemNumber = problemNumber + (pow(10, (i - j)) *  (fileName[j] - '0'));
+        // printf("i = %i, j = %i, fileName[%i] = %c, problemNumber = %i \n", i, j, j, fileName[j], problemNumber);
+    }
+
+    printf("Problem number %i: %s\n", problemNumber, problemName);
 }
